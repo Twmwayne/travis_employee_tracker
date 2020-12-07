95ff93f459f48653fa79e9e02784442d0b264e37
Travis Mendenhall <twmwayne@gmail.com>
2020-12-07 13:24:52 -0700

    created connections, began main menu function

--- employee-tracker.js
+++ employee-tracker.js
@@ -11,3 +11,44 @@ const connectionProperties = {
     password: "password",
     database: "employees_DB"
 }
+
+
+// Creating Connection
+const connection = mysql.createConnection(connectionProperties);
+
+
+// Establishing Connection to database
+connection.connect((err) => {
+    if (err) throw err;
+
+    // Start main menu function
+
+    console.log("\n WELCOME TO EMPLOYEE TRACKER \n");
+    mainMenu();
+});
+
+// Main menu function
+function mainMenu(){
+
+    // Prompt user to choose an option
+    inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "MAIN MENU",
+      choices: [
+        "View all employees",
+        "View all employees by role",
+        "View all employees by department",
+        "View all employees by manager",
+        "Add employee",
+        "Add role",
+        "Add department",
+        "Update employee role",
+        "Update employee manager",
+        "Delete employee",
+        "Delete role",
+        "Delete department",
+        "View department budgets"
+      ]
+    })
