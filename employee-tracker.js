8f14c9add627e0896a70b2e0053e6814b806ada4
Travis Mendenhall <twmwayne@gmail.com>
2020-12-07 13:03:00 -0700

    added const for required, and connection properties to js file

---
+++ employee-tracker.js
@@ -0,0 +1,13 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const consoleTable = require("console.table");
+const promisemysql = require("promise-mysql");
+
+// Connection Properties
+const connectionProperties = {
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "employees_DB"
+}
